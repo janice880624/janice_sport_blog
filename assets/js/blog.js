adfc49cd8ca0a37b17b89ec26550f8908488dd86
janice880624 <t106300302@ntut.org.tw>
2024-02-18 21:07:00 +0800

    0218

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -71,5 +71,72 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
+  document.getElementById('showAllMobile').addEventListener('click', function() {
+    var items = document.querySelectorAll('.item');
+    items.forEach(function(item) {
+        item.style.display = ''; // 顯示所有文章
+    });
+  }); 
+
+  document.getElementById('showBigMobile').addEventListener('click', function() {
+      // 顯示所有游泳類別的文章
+      var items = document.querySelectorAll('.item');
+      items.forEach(function(item) {
+          if(item.classList.contains('big')) {
+              item.style.display = ''; // 或者 "block"，根據你的需要
+          } else {
+              item.style.display = 'none';
+          }
+      });
+  });
+
+  document.getElementById('showSwimMobile').addEventListener('click', function() {
+     // 顯示所有游泳類別的文章
+      var items = document.querySelectorAll('.item');
+      items.forEach(function(item) {
+          if(item.classList.contains('swim')) {
+              item.style.display = ''; // 或者 "block"，根據你的需要
+          } else {
+              item.style.display = 'none';
+          }
+      });
+  });
+
+  document.getElementById('showBicycleMobile').addEventListener('click', function() {
+    // 顯示所有游泳類別的文章
+    var items = document.querySelectorAll('.item');
+    items.forEach(function(item) {
+        if(item.classList.contains('bicycle')) {
+            item.style.display = ''; // 或者 "block"，根據你的需要
+        } else {
+            item.style.display = 'none';
+        }
+    });
+  });
+
+  document.getElementById('showRunMobile').addEventListener('click', function() {
+    // 顯示所有游泳類別的文章
+    var items = document.querySelectorAll('.item');
+    items.forEach(function(item) {
+        if(item.classList.contains('run')) {
+            item.style.display = ''; // 或者 "block"，根據你的需要
+        } else {
+            item.style.display = 'none';
+        }
+    });
+  });
+
+  document.getElementById('showChangeMobile').addEventListener('click', function() {
+    // 顯示所有游泳類別的文章
+    var items = document.querySelectorAll('.item');
+    items.forEach(function(item) {
+        if(item.classList.contains('change')) {
+            item.style.display = ''; // 或者 "block"，根據你的需要
+        } else {
+            item.style.display = 'none';
+        }
+    });
+  });
+
 
 });
