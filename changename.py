79e18fa9b0482e5c1f32d6c58fcf853c5b3a6e33
janice880624 <t106300302@ntut.org.tw>
2024-04-16 12:54:54 +0800

    0409

--- changename.py
+++ changename.py
@@ -20,6 +20,6 @@ def rename_photos(directory, prefix):
 
 # 使用範例
 # directory_path = '/Users/janice/Desktop/janice_sport_blog/blog/other/pic/0218'
-directory_path = 'blog/TRBB/pic/0317'
-prefix = '0317-'
+directory_path = 'blog/TRBB/pic/0409'
+prefix = '0409-'
 rename_photos(directory_path, prefix)
