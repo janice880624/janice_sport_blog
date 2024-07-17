7a8d71e0058f8631ab9aa142a914515497ce7e24
janice880624 <t106300302@ntut.org.tw>
2024-07-17 21:39:26 +0800

    0616

--- changename.py
+++ changename.py
@@ -4,11 +4,11 @@ def rename_photos(directory, prefix):
     # 獲取指定目錄下所有的文件
     files = os.listdir(directory)
     # 過濾出.jpg結尾的文件，你可以根據需要修改擴展名
-    photos = [file for file in files if file.lower().endswith('.png')]
+    photos = [file for file in files if file.lower().endswith('.jpg')]
     # 排序檔案名稱，確保按照原有順序重新命名
     photos.sort()
     # 遍歷所有照片並重新命名
-    for index, photo in enumerate(photos, start=14):
+    for index, photo in enumerate(photos, start=1):
         # 建立新的文件名
         new_name = f"{prefix}{index}.jpg"
         # 建立原始和新的完整路徑
@@ -19,7 +19,7 @@ def rename_photos(directory, prefix):
         print(f"Renamed '{photo}' to '{new_name}'")
 
 # 使用範例
-# directory_path = '/Users/janice/Desktop/janice_sport_blog/blog/other/pic/0218'
-directory_path = '/Users/janice/Desktop/janice_sport_blog/blog/other/pic/0716'
-prefix = '0716-'
+directory_path = '/Users/janice/Desktop/janice_sport_blog/blog/other/pic/0616'
+# directory_path = '/Users/janice/Desktop/janice_sport_blog/blog/TRBB/pic/0623'
+prefix = '0616-'
 rename_photos(directory_path, prefix)
